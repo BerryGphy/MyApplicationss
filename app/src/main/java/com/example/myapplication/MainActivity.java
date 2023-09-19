a12a9432d1b605ad68a7868e3dbc4221008123f1
BerryGphy <hugo.berry19@gmail.com>
2023-09-19 14:03:47 +0200

    Un message

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private EditText editTextNumberDecimal;
     private RadioGroup radioGroup;
-    private RadioButton radio_Fareinheit;
+    private RadioButton radio_Fahreiheit;
     private RadioButton radio_Celcius;
     private Button button2;
     private TextView label_finalResult;
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
         editTextNumberDecimal = findViewById(R.id.editTextNumberDecimal);
         radioGroup = findViewById(R.id.radioGroup);
-        radio_Fareinheit = findViewById(R.id.radio_Fareinheit);
+        radio_Fahreiheit = findViewById(R.id.radio_Fahreiheit);
         radio_Celcius = findViewById(R.id.radio_Celcius);
         button2 = findViewById(R.id.button2);
         label_finalResult = findViewById(R.id.label_finalResult);
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     }
     private void calculateResult() {
         double inputValue = Double.parseDouble(editTextNumberDecimal.getText().toString());
-        if (radio_Fareinheit.isChecked()) {
+        if (radio_Fahreiheit.isChecked()) {
             inputValue = (inputValue-32)*5/9;
             label_finalResult.setText(inputValue + "°C");
         } else if (radio_Celcius.isChecked()) {
