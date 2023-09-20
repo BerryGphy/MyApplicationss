a7053b044c33402f1a748fe4fae385db1f02b042
BerryGphy <hugo.berry19@gmail.com>
2023-09-20 15:18:50 +0200

    KWAK KWAK

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,23 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.VibrationEffect;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.RadioGroup;
-import android.widget.RadioButton;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.os.Vibrator;
 import android.media.MediaPlayer;
 public class MainActivity extends AppCompatActivity {
     private static final int DEFAULT_AMPLITUDE = 100;
     private EditText editTextNumberDecimal; // ID de l'editText
-    private RadioGroup radioGroup;
-    private RadioButton radio_Fahreiheit; // ID du radio bouton F°
-    private RadioButton radio_Celcius; // ID du radio bouton C°
     private Button button2; // ID du bouton convertir
     private TextView label_finalResult; // ID du texte
     private MediaPlayer mediaPlayer;
+    private Switch switchAuto;
+    private Spinner spinnerEntry;
+    private Spinner spinnerExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +33,65 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mediaPlayer = MediaPlayer.create(this, R.raw.kwak);
         editTextNumberDecimal = findViewById(R.id.editTextNumberDecimal);
-        radioGroup = findViewById(R.id.radioGroup);
-        radio_Fahreiheit = findViewById(R.id.radio_Fahreiheit);
-        radio_Celcius = findViewById(R.id.radio_Celcius);
         button2 = findViewById(R.id.button2);
         label_finalResult = findViewById(R.id.label_finalResult);
-        //button2.setOnClickListener(view -> calculateResult());
+        spinnerEntry = findViewById(R.id.spinnerEntry);
+        spinnerExit = findViewById(R.id.spinnerExit);
+        switchAuto = findViewById(R.id.switchAuto);
+        switchAuto.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Appel de la méthode de conversion automatique lorsque le switch est activé
+                if (isChecked) {
+                    performAutoConversion();
+                }
+            }
+        });
+        spinnerEntry.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // Appel de la méthode de conversion automatique lorsque la sélection change
+                if (switchAuto.isChecked()) {
+                    performAutoConversion();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+            }
+        });
+
+        spinnerExit.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // Appel de la méthode de conversion automatique lorsque la sélection change
+                if (switchAuto.isChecked()) {
+                    performAutoConversion();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+            }
+        });
+        editTextNumberDecimal.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2){
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (switchAuto.isChecked()) {
+                    performAutoConversion();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+
+
     }
 
     public void vibrate(int duration){
@@ -48,21 +105,98 @@ public class MainActivity extends AppCompatActivity {
         calculateResult();
     }
     private void calculateResult() {
-        double inputValue = Double.parseDouble(editTextNumberDecimal.getText().toString());
-        if (radio_Fahreiheit.isChecked()) {
-            inputValue = (inputValue-32)*5/9;
-            label_finalResult.setText(inputValue + "°C");
-        } else if (radio_Celcius.isChecked()) {
-            inputValue = (inputValue*9/5)+32;
-            label_finalResult.setText(inputValue + "°F");
+        double inputValue = 0.0;
+        try {
+            inputValue = Double.parseDouble(editTextNumberDecimal.getText().toString());
+        } catch (NumberFormatException e) {
+            label_finalResult.setText("Entrée invalide");
+            return;
+        }
+        String itemEntry = spinnerEntry.getSelectedItem().toString();
+        String itemExit = spinnerExit.getSelectedItem().toString();
+        if (itemEntry.equals(itemExit)){
+            label_finalResult.setText(inputValue+"");
+        } else if ("°F".equals(itemEntry)){
+            if("°K".equals(itemExit)){
+                inputValue = (inputValue + 459.67)/1.8;
+                label_finalResult.setText(inputValue+"");
+            } else if ("°C".equals(itemExit)) {
+                inputValue = (inputValue-32)*5/9;
+                label_finalResult.setText(inputValue +"");
+            }
+        } else if ("°K".equals(itemEntry)) {
+            if("°F".equals(itemExit)){
+                inputValue = inputValue*1.8-459.67;
+                label_finalResult.setText(inputValue+"");
+            } else if ("°C".equals(itemExit)) {
+                inputValue = inputValue-273.15;
+                label_finalResult.setText(inputValue+"");
+            }
+        } else if ("°C".equals(itemEntry)){
+            if("°K".equals(itemExit)){
+                inputValue = inputValue + 273.15;
+                label_finalResult.setText(inputValue+"");
+            } else if ("°F".equals(itemExit)) {
+                inputValue = (inputValue*9/5)+32;
+                label_finalResult.setText(inputValue+"");
+            }
+        } else {
+            label_finalResult.setText("erreur");
         }
         vibrate(1000);
         playButtonClickSound();
     }
+    private void performAutoConversion() {
+        double inputValue = 0.0;
+        try {
+            inputValue = Double.parseDouble(editTextNumberDecimal.getText().toString());
+        } catch (NumberFormatException e) {
+            label_finalResult.setText("Entrée invalide");
+            return;
+        }
+
+        String itemEntry = spinnerEntry.getSelectedItem().toString();
+        String itemExit = spinnerExit.getSelectedItem().toString();
+
+        if (itemEntry.equals(itemExit)) {
+            label_finalResult.setText(inputValue + "");
+        } else if ("°F".equals(itemEntry)) {
+            if ("°K".equals(itemExit)) {
+                inputValue = (inputValue + 459.67) / 1.8;
+                label_finalResult.setText(inputValue + "");
+            } else if ("°C".equals(itemExit)) {
+                inputValue = (inputValue - 32) * 5 / 9;
+                label_finalResult.setText(inputValue + "");
+            }
+        } else if ("°K".equals(itemEntry)) {
+            if ("°F".equals(itemExit)) {
+                inputValue = inputValue * 1.8 - 459.67;
+                label_finalResult.setText(inputValue + "");
+            } else if ("°C".equals(itemExit)) {
+                inputValue = inputValue - 273.15;
+                label_finalResult.setText(inputValue + "");
+            }
+        } else if ("°C".equals(itemEntry)) {
+            if ("°K".equals(itemExit)) {
+                inputValue = inputValue + 273.15;
+                label_finalResult.setText(inputValue + "");
+            } else if ("°F".equals(itemExit)) {
+                inputValue = (inputValue * 9 / 5) + 32;
+                label_finalResult.setText(inputValue + "");
+            }
+        } else {
+            label_finalResult.setText("Erreur");
+        }
+
+        vibrate(1000);
+        playButtonClickSound();
+    }
+
     private void playButtonClickSound() {
         if (mediaPlayer != null) {
-            mediaPlayer.seekTo(0); // Réinitialiser la position de lecture
-            mediaPlayer.start(); // Commencer la lecture du son
+            mediaPlayer.seekTo(2500); // Réinitialiser la position de lecture
+            mediaPlayer.start();
         }
     }
+
 }
